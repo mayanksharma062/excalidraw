d2807dbbdec6f727bedf1037e2b34f5a27762254
Mayank Sharma <sharma.mayank3011@gmail.com>
2025-05-28 04:14:40 +0530

    set http server and create signup, signIn and createRoom API endpoints

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,41 @@
 import express from "express";
+import jwt from "jsonwebtoken"
+import { middleware } from "./middleware";
 const app = express();
+
+app.post("/signup", (req, res)=>{
+
+  // db Call
+
+  res.json({
+    auth: "123123";
+  })
+  
+})
+
+app.post("/signin", (req, res)=>{
+
+  const userId = 1;
+  const token = jwt.sign({
+    userId
+  }, "123123");
+
+
+  res.json({
+    token
+  })
+
+})
+
+app.post("/room",middleware, (req, res)=>{
+
+  // dbCall
+
+
+  res.json({
+    roomId: "123";
+  })
+})
 
 app.listen(3001, ()=>{
   console.log("Everything working fine...");
