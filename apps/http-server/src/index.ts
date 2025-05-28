3926d3cd68c49c8d9cafd5d404f138e5497e5020
Mayank Sharma <sharma.mayank3011@gmail.com>
2025-05-28 19:35:25 +0530

    Added websocket init logic

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -8,12 +8,12 @@ app.post("/signup", (req, res)=>{
   // db Call
 
   res.json({
-    auth: "123123";
+    userId: "123123"
   })
   
 })
 
-app.post("/signin", (req, res)=>{
+app.post("/signIn", (req, res)=>{
 
   const userId = 1;
   const token = jwt.sign({
@@ -33,7 +33,7 @@ app.post("/room",middleware, (req, res)=>{
 
 
   res.json({
-    roomId: "123";
+    roomId: "123"
   })
 })
 
---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,26 @@
+import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+
+  if(!url){
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, "123123");
+
+  if(!decoded || !(decoded as JwtPayload).userId){
+    ws.close();
+    return;
+  }
+
+  ws.on('message', function message(data) {
+    ws.send('something');
+  });
+
+});
