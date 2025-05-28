df0ec000ca73419540cd48420c50c3c5c67ade2b
Mayank Sharma <sharma.mayank3011@gmail.com>
2025-05-28 20:03:58 +0530

    Added support for zod, Added required common packages

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,24 +1,45 @@
 import express from "express";
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types"
+
 const app = express();
 
 app.post("/signup", (req, res)=>{
 
+  const data = CreateUserSchema.safeParse(req.body);
+
+  if(!data.success){
+    res.json({
+      message: "Incorrect Inputs"
+    })
+    return;
+  }
+
   // db Call
 
   res.json({
-    userId: "123123"
+    userId: JWT_SECRET
   })
   
 })
 
 app.post("/signIn", (req, res)=>{
 
+  const data = SignInSchema.safeParse(req.body);
+
+  if(!data.success){
+    res.json({
+      message: "Incorrect Inputs"
+    })
+    return;
+  }
+
   const userId = 1;
   const token = jwt.sign({
     userId
-  }, "123123");
+  }, JWT_SECRET);
 
 
   res.json({
@@ -28,6 +49,15 @@ app.post("/signIn", (req, res)=>{
 })
 
 app.post("/room",middleware, (req, res)=>{
+
+  const data = CreateRoomSchema.safeParse(req.body);
+
+  if(!data.success){
+    res.json({
+      message: "Incorrect Inputs"
+    })
+    return;
+  }
 
   // dbCall
 
--- apps/http-server/src/middleware.ts
+++ apps/http-server/src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 export function middleware(req: Request, res: Response, next: NextFunction){
 
   const token = req.headers["authorization"] ?? "";
 
-  const decoded = jwt.verify(token, "123123")
+  const decoded = jwt.verify(token, JWT_SECRET)
   if(decoded){
     // @ts-ignore: TODO: Fix this
     req.userId = decoded.userId;
